8a941c0a2a3fe8a3da5c66e1de189fab757feeed
Celeste <yuan.zx@outlook.com>
2019-12-07 16:32:22 +0800

    fix permission

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
+npm install
+
 build_paths=(
   learn-data-structure
 )
 
 for path in ${build_paths[@]}; do
   echo "build ${path}"
-  npm run docs:build ${path}
+  npm run build ${path}
 done
