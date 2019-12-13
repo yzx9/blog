385b2a0e7b982475905ebbf2969482c2c35a5741
Celeste <yuan.zx@outlook.com>
2019-12-13 12:27:45 +0800

    add note about network

---
+++ network/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  base: "/network/",
+  title: "Great internet",
+  plugins: [
+    [
+      "vuepress-plugin-mathjax",
+      {
+        target: "svg",
+        macros: {
+          "*": "\\times"
+        }
+      }
+    ]
+  ],
+  themeConfig: {
+    lastUpdated: "Last Updated",
+    sidebar: ["http.md"]
+  }
+};
