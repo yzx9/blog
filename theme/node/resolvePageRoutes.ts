835e0aea8281b685a684175e11a5a99b292cff85
Celeste <yuan.zx@outlook.com>
2021-02-03 16:36:05 +0800

    fix(theme): remove useless code

--- theme/node/resolvePageRoutes.ts
+++ theme/node/resolvePageRoutes.ts
@@ -1,7 +1,6 @@
 import type { App } from "@vuepress/core"
 
 export const resolvePageRoutes = (app: App) => {
-  app.pages.forEach((a) => a.date)
   app.pages
     .filter((a) => a.slug)
     .forEach((a) => (a.path = `/posts/${a.slug}.html`))
