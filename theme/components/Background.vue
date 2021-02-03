3fb0e8278ff1b9ad38dd21a3414a1bf9dff03f54
Celeste <yuan.zx@outlook.com>
2021-02-03 13:49:15 +0800

    faet(theme): add particles background

--- theme/components/Background.vue
+++ theme/components/Background.vue
@@ -1,12 +1,29 @@
 <template>
-  <div class="background"></div>
+  <div class="background">
+    <div id="background__particles" class="background__particles" />
+  </div>
 </template>
 
 <script lang="ts">
-import { reactive, ref } from "vue"
+import { ref, onMounted, onBeforeUnmount } from "vue"
+import { tsParticles } from "tsparticles"
+import type { Container, ISourceOptions } from "tsparticles"
+import ParticlePresetNasa from "../assets/particles-preset-nasa.json"
 
 export default {
   setup(props, ctx) {
+    const particlesOptions = ParticlePresetNasa as ISourceOptions
+    const containerRef = ref<Container | undefined>(undefined)
+
+    onMounted(async () => {
+      containerRef.value = await tsParticles.load(
+        "background__particles",
+        particlesOptions
+      )
+    })
+
+    onBeforeUnmount(() => containerRef.value?.destroy())
+
     return {}
   },
 }
