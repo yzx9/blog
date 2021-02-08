6310878d71ba87fdec680656b1df747a7df5a6f2
Celeste <yuan.zx@outlook.com>
2021-02-09 01:38:01 +0800

    feat(theme): update home page

--- theme/layouts/Home.vue
+++ theme/layouts/Home.vue
@@ -1,11 +1,57 @@
 <template>
-  <Background />
+  <teleport to="body">
+    <nav class="layout__navbar">
+      <Navbar />
+    </nav>
+  </teleport>
+
+  <div class="layout">
+    <header class="layout__header">
+      <div class="header">
+        <h1 class="header__title header__title--primary">{{ title }}</h1>
+        <h2 class="header__description">{{ description }}</h2>
+        <Particles class="header__background" preset="starry" />
+      </div>
+    </header>
+
+    <div class="layout__wrapper">
+      <aside class="layout__sidebar">
+        <!-- <Sidebar /> -->
+      </aside>
+      <main class="layout__page">
+        <!-- <Page /> -->
+      </main>
+      <aside class="layout__catalog">
+        <!-- <Catalog /> -->
+      </aside>
+    </div>
+
+    <footer class="layout__footer">
+      <Footer />
+    </footer>
+  </div>
 </template>
 
 <script lang="ts">
-import Background from "../components/Background.vue"
+import { useSiteLocaleData } from "@vuepress/client"
+import Navbar from "../components/Navbar.vue"
+import Footer from "../components/Footer.vue"
+import Particles from "../components/Particles.vue"
 
 export default {
-  components: { Background },
+  components: {
+    Navbar,
+    Footer,
+    Particles,
+  },
+  setup(props, ctx) {
+    const siteLocaleData = useSiteLocaleData()
+    const { title, description } = siteLocaleData.value
+
+    return {
+      title,
+      description,
+    }
+  },
 }
 </script>
--- vuepress.config.ts
+++ vuepress.config.ts
@@ -2,14 +2,25 @@ import { path } from "@vuepress/utils"
 import type { UserConfig } from "@vuepress/cli"
 import type { ThemeOptions } from "./theme/types"
 
+const FontPath = [
+  "//fonts.googleapis.com/css?family=",
+  "Monda:300,300italic,400,400italic,700,700italic|",
+  "Roboto Slab:300,300italic,400,400italic,700,700italic|",
+  "Microsoft YaHei:300,300italic,400,400italic,700,700italic|",
+  "Kaushan Script:300,300italic,400,400italic,700,700italic|",
+  "PT Mono:300,300italic,400,400italic,700,700italic&amp;subset=latin,latin-ext",
+].join("")
+
 const config: UserConfig<ThemeOptions> = {
   title: "Celeste's blog",
   description: "Technique && Life",
 
   lang: "zh-CN",
   base: process.env.BASE_PATH || "/",
-  head: [["link", { rel: "icon", href: `/logo.png` }]],
-
+  head: [
+    ["link", { rel: "icon", href: `/logo.png` }],
+    ["link", { rel: "stylesheet", href: FontPath }],
+  ],
   dest: "dist",
   temp: ".temp",
   cache: ".cache",
