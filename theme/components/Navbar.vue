fb2450028f95eefc87fa9f6a3e264fc15e8d47bd
Celeste <yuan.zx@outlook.com>
2021-02-09 13:25:29 +0800

    feat(theme): adjust ui

--- theme/components/Navbar.vue
+++ theme/components/Navbar.vue
@@ -8,10 +8,7 @@
     }"
   >
     <ul class="navbar__list">
-      <li
-        class="navbar__link navbar__link--homepage"
-        :class="{ 'navbar__link--active': active.blog }"
-      >
+      <li class="navbar__link navbar__link--homepage">
         <RouterLink to="/">{{ title }}</RouterLink>
       </li>
     </ul>
@@ -43,7 +40,7 @@
 </template>
 
 <script lang="ts">
-import { computed, reactive, ref, watch } from "vue"
+import { computed, onMounted, reactive, ref, watch } from "vue"
 import { useRouter } from "vue-router"
 import { useSiteLocaleData } from "@vuepress/client"
 import { useScroll } from "../composables"
@@ -54,9 +51,24 @@ const timespan = 500
 
 export default {
   setup(props, ctx) {
+    // active link
+    const { currentRoute } = useRouter()
+    const active = computed(() => {
+      const { path } = currentRoute.value
+      return {
+        home: path === "/",
+        archives: path.startsWith("/archives"),
+        categories: path.startsWith("/categories"),
+        tags: path.startsWith("/tags"),
+        about: path.startsWith("/about"),
+      }
+    })
+
     // title
     const siteLocaleData = useSiteLocaleData()
-    const title = ref(siteLocaleData.value.title.toUpperCase())
+    const title = computed(() =>
+      active.value.home ? "BLOG" : siteLocaleData.value.title.toUpperCase()
+    )
 
     // display
     const status = reactive({
@@ -78,49 +90,37 @@ export default {
     let lastTop = 0
 
     watch(useScroll(), ({ top }) => {
-      const now = Date.now()
+        const now = Date.now()
 
-      if (top <= headerHeight) {
-        status.opaque = false
-        status.hidden = false
-        status.instant = true
-        lastTime = now
-      } else if (lastTop <= headerHeight) {
-        status.opaque = true
-        status.hidden = true
-        status.instant = true
-        lastTime = now
-      } else {
-        status.opaque = true
-
-        if (now - lastTime > timespan) {
-          const offset = top - lastTop
-          status.instant = false
-
-          if (offset > threshold) {
-            status.hidden = true
-          } else if (offset < -threshold) {
-            status.hidden = false
-          }
-
+        if (top <= headerHeight) {
+          status.opaque = false
+          status.hidden = false
+          status.instant = true
           lastTime = now
+        } else if (lastTop <= headerHeight) {
+          status.opaque = true
+          status.hidden = true
+          status.instant = true
+          lastTime = now
+        } else {
+          status.opaque = true
+
+          if (now - lastTime > timespan) {
+            const offset = top - lastTop
+            status.instant = false
+
+            if (offset > threshold) {
+              status.hidden = true
+            } else if (offset < -threshold) {
+              status.hidden = false
+            }
+
+            lastTime = now
+          }
         }
-      }
 
-      lastTop = top
-    })
-
-    // Active link
-    const { currentRoute } = useRouter()
-    const active = computed(() => {
-      const path = currentRoute.value.path
-      return {
-        home: path === "/",
-        archives: path.startsWith("/archives"),
-        categories: path.startsWith("/categories"),
-        tags: path.startsWith("/tags"),
-        about: path.startsWith("/about"),
-      }
+        lastTop = top
+      })
     })
 
     return {
