48b9a8ca5a75959ba9755be63e9944c7d76541a3
Celeste <yuan.zx@outlook.com>
2021-05-09 20:54:20 +0800

    refactor(plugin-post-filter): rebuild workspace

--- packages/plugin-post-filter/src/filter.ts
+++ packages/plugin-post-filter/src/filter.ts
@@ -1,5 +1,4 @@
-import type { App, Page, PluginFunction } from "@vuepress/core"
-import { PostFilterOptions } from "./type"
+import type { Page } from "@vuepress/core"
 
 const name = "vuepress-plugin-post-filter"
 
@@ -9,31 +8,4 @@ export const createPostFilter = (frontmatter: Record<string, any>) => {
   )
 
   return (page: Page) => testFns.every((fn) => fn(page))
-}
-
-export const postFilterPluginRaw: PluginFunction = (
-  options: PostFilterOptions = {}
-) => {
-  const { frontmatter = { draft: true, published: false } } = options
-
-  const filter = createPostFilter(frontmatter)
-
-  const onInitialized = (app: App) => {
-    app.pages = app.pages.filter(filter)
-  }
-
-  return {
-    name,
-    onInitialized,
-  }
-}
-
-export const postFilterPlugin: PluginFunction = (
-  options: PostFilterOptions = {},
-  app
-) => {
-  const { prodOnly = true } = options
-  return prodOnly && app.env.isProd
-    ? postFilterPluginRaw(options, app)
-    : { name }
 }
--- packages/plugin-post-filter/src/index.ts
+++ packages/plugin-post-filter/src/index.ts
@@ -1,7 +1,40 @@
-import { postFilterPlugin } from "./filter"
+import { createPostFilter } from "./filter"
+import type { App, Plugin, PluginFunction } from "@vuepress/core"
+import type { PostFilterOptions } from "./type"
 
+const name = "vuepress-plugin-post-filter"
+
+/**
+ * Post Filter for Vueress, Work for both mode
+ * @param options
+ * @returns
+ */
+export const VuepressPluginPostFilterRaw: PluginFunction<PostFilterOptions> = (
+  options = {}
+) => {
+  const { frontmatter = { draft: true, published: false } } = options
+
+  const filter = createPostFilter(frontmatter)
+
+  const onInitialized = (app: App) => {
+    app.pages = app.pages.filter(filter)
+  }
+
+  return {
+    name,
+    onInitialized,
+  }
+}
+
+export const VuepressPluginPostFilter: Plugin<PostFilterOptions> = (
+  options = {},
+  app
+) => {
+  const { prodOnly = true } = options
+  return prodOnly && app.env.isProd
+    ? VuepressPluginPostFilterRaw(options, app)
+    : { name }
+}
+
+export default VuepressPluginPostFilter
 export * from "./type"
-export * from "./filter"
-
-export { postFilterPlugin as postFilter }
-export default postFilterPlugin
