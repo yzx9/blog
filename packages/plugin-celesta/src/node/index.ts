6be9c32b2d223dbc2636c28fc21d16ad32add04e
Celeste <yuan.zx@outlook.com>
2021-05-10 19:14:44 +0800

    refactor(plugin-celesta): add complie workflow

--- packages/plugin-celesta/src/node/index.ts
+++ packages/plugin-celesta/src/node/index.ts
@@ -1,2 +1,17 @@
-export * from "./extendsPageData"
-export * from "./onPrepare"
+import { path } from "@vuepress/utils"
+import { extendsPageData } from "./extendsPageData"
+import { createPreparedHook } from "./onPrepare"
+import type { Plugin } from "@vuepress/core"
+import type { ThemeData } from "../types"
+
+export const VuepressPluginCelesta: Plugin<ThemeData> = (options, app) => {
+  return {
+    name: "vuepress-plugin-celesta",
+    clientAppSetupFiles: path.resolve(__dirname, "../client/clientAppSetup.ts"),
+    extendsPageData,
+    onPrepared: createPreparedHook(options),
+  }
+}
+
+export default VuepressPluginCelesta
+export * from "../types"
