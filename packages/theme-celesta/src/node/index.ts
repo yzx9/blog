5887373e2491de6c72cc9811030c6696da32a51d
Celeste <yuan.zx@outlook.com>
2021-05-10 19:37:51 +0800

    refactor(theme-celesta): move entry file to node side

--- packages/theme-celesta/src/node/index.ts
+++ packages/theme-celesta/src/node/index.ts
@@ -1,7 +1,40 @@
-export * from "./assignDefaultOptions"
+import { path } from "@vuepress/utils"
+import { assignDefaultOptions } from "./assignDefaultOptions"
+import { extendsPageOptions } from "./extendsPageOptions"
+import { onInitialized } from "./onInitialized"
+import type { Theme } from "@vuepress/core"
+import type { ThemeData } from "../types"
 
-export * from "./extendsPageOptions"
+export const VuepressThemeCelesta: Theme<ThemeData> = (themeOptions, app) => {
+  assignDefaultOptions(themeOptions, app.options.lang)
 
-export * from "./onInitialized"
-export * from "./initializeHomePage"
-export * from "./initializeThemePages"
+  return {
+    name: "vuepress-theme-celesta",
+
+    plugins: [
+      "@vuepress/plugin-debug",
+      [
+        "@vuepress/plugin-active-header-links",
+        { headerLinkSelector: ".catalog__link" },
+      ],
+      ["@vuepress/theme-data", { themeData: themeOptions }],
+      ["vuepress-plugin-celesta", themeOptions],
+      "vuepress-plugin-post-filter",
+      "vuepress-plugin-math",
+    ],
+
+    layouts: path.resolve(__dirname, "../client/layouts"),
+
+    clientAppEnhanceFiles: path.resolve(
+      __dirname,
+      "../client/clientAppEnhance.ts"
+    ),
+
+    extendsPageOptions,
+
+    onInitialized,
+  }
+}
+
+export default VuepressThemeCelesta
+export * from "../types"
