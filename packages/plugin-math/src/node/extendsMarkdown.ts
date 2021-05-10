1dd391a8676a7c011344ab5ed01acdf717d22e71
Celeste <yuan.zx@outlook.com>
2021-05-10 22:58:06 +0800

    refactor(plugin-math): move entry

--- packages/plugin-math/src/node/extendsMarkdown.ts
+++ packages/plugin-math/src/node/extendsMarkdown.ts
@@ -1,6 +1,6 @@
+import { math_plugin } from "./markdownItPlugin"
 import type { App } from "@vuepress/core"
 import type * as MarkdownIt from "markdown-it"
-import { math_plugin } from "./markdownItPlugin"
 
 export const extendsMarkdown = (md: MarkdownIt, app: App) => {
   md.use(math_plugin)
--- packages/plugin-math/src/node/index.ts
+++ packages/plugin-math/src/node/index.ts
@@ -1,1 +1,16 @@
-export * from "./extendsMarkdown"
+import { path } from "@vuepress/utils"
+import { extendsMarkdown } from "./extendsMarkdown"
+import type { PluginObject } from "@vuepress/core"
+
+export const VuepressPluginMath: PluginObject = {
+  name: "vuepress-plugin-math",
+
+  clientAppEnhanceFiles: path.resolve(
+    __dirname,
+    "../client/clientAppEnhance.js"
+  ),
+
+  extendsMarkdown,
+}
+
+export default VuepressPluginMath
