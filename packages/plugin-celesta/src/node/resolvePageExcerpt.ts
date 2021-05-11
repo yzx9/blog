dbf213913733e618aa5c2d7650e26c823765b9b4
Celeste <yuan.zx@outlook.com>
2021-05-12 00:00:26 +0800

    style(plugin-celesta): improve code structure

--- packages/plugin-celesta/src/node/resolvePageExcerpt.ts
+++ packages/plugin-celesta/src/node/resolvePageExcerpt.ts
@@ -4,22 +4,17 @@ import type { Page } from "@vuepress/core"
 const ExcerptLength = 200
 
 export const resolvePageExcerpt = (page: Page) => {
-  if (page.excerpt) {
-    return removeMarkdown(page.excerpt)
-  }
+  if (page.excerpt) return removeMarkdown(page.excerpt)
 
-  const content = page.content
-  if (!content) {
-    return ""
-  }
+  const { content } = page
+  if (!content) return ""
 
-  const excerpt =
-    removeMarkdown(
-      content
-        .trim()
-        .replace(/^#+\s+(.*)/, "")
-        .slice(0, ExcerptLength)
-    ) + " ..."
+  const slice = content
+    .trim()
+    .replace(/^#+\s+(.*)/, "")
+    .slice(0, ExcerptLength)
+
+  const excerpt = removeMarkdown(slice) + " ..."
 
   return excerpt
 }
