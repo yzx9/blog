9df66d0e01d83291e5b77366d4b176b09bc64423
Celeste <yuan.zx@outlook.com>
2021-06-05 18:22:51 +0800

    pref(theme-celesta): use only teleport

--- packages/theme-celesta/src/client/layouts/BaseLayout.vue
+++ packages/theme-celesta/src/client/layouts/BaseLayout.vue
@@ -28,9 +28,7 @@
           <TheNavbar />
         </slot>
       </nav>
-    </Teleport>
 
-    <Teleport to="#app">
       <ClientOnly>
         <Particles class="layout__background" preset="starry" />
       </ClientOnly>
