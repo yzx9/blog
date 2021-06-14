47be4b67eb912036ac67670291cd0035b1028652
Celeste <yuan.zx@outlook.com>
2021-06-14 11:30:07 +0800

    feat(theme-celesta): use css custom property as theme colors

--- packages/theme-celesta/src/node/assignPostcssConfig.ts
+++ packages/theme-celesta/src/node/assignPostcssConfig.ts
@@ -1,4 +1,3 @@
-import { cyan, pink } from "tailwindcss/colors"
 import type { App } from "@vuepress/core"
 import type { ProcessOptions, Plugin } from "postcss"
 import type { WebpackBundlerOptions } from "@vuepress/bundler-webpack"
@@ -17,8 +16,30 @@ export function assignPostcssConfig(app: App) {
     theme: {
       extend: {
         colors: {
-          primary: cyan,
-          secondary: pink,
+          primary: {
+            50: "var(--color-primary-50)",
+            100: "var(--color-primary-100)",
+            200: "var(--color-primary-200)",
+            300: "var(--color-primary-300)",
+            400: "var(--color-primary-400)",
+            500: "Var(--color-primary-500)",
+            600: "var(--color-primary-600)",
+            700: "var(--color-primary-700)",
+            800: "var(--color-primary-800)",
+            900: "var(--color-primary-900)",
+          },
+          secondary: {
+            50: "var(--color-secondary-50)",
+            100: "var(--color-secondary-100)",
+            200: "var(--color-secondary-200)",
+            300: "var(--color-secondary-300)",
+            400: "var(--color-secondary-400)",
+            500: "Var(--color-secondary-500)",
+            600: "var(--color-secondary-600)",
+            700: "var(--color-secondary-700)",
+            800: "var(--color-secondary-800)",
+            900: "var(--color-secondary-900)",
+          },
         },
       },
     },
@@ -39,6 +60,7 @@ export function assignPostcssConfig(app: App) {
         : []),
     ],
   }
+
   const webpackBundlerOptions: WebpackBundlerOptions = {
     postcss: {
       postcssOptions: postcssConfig,
