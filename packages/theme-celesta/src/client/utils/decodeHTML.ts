dcc1b3bb8fc6effd7743fff1e84d26a4202e2f05
Celeste <yuan.zx@outlook.com>
2022-03-24 21:21:58 +0800

    fix: rename ssr flg

--- packages/theme-celesta/src/client/utils/decodeHTML.ts
+++ packages/theme-celesta/src/client/utils/decodeHTML.ts
@@ -1,8 +1,8 @@
 // TODO: remove SSR flag
-declare const __SSR__: boolean
+declare const __VUEPRESS_SSR__: boolean
 
 export function decodeHTML(input: string) {
-  if (__SSR__ === true) return input
+  if (__VUEPRESS_SSR__ === true) return input
   const doc = new DOMParser().parseFromString(input, "text/html")
   return doc.documentElement.textContent
 }
