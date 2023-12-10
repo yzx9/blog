a6f61981544dab332167a3d59d54d08d2c43f76b
yzx9 <yuan.zx@outlook.com>
2023-12-10 12:46:12 +0800

    feat: add math and mermaid support

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,17 +1,23 @@
 import { defineConfigWithTheme } from "vitepress"
+import { withMermaid } from "vitepress-plugin-mermaid"
 import { type Config } from "vitepress-theme-celesta"
 
-export default defineConfigWithTheme<Config>({
-  head: [["link", { rel: "icon", href: "/logo.ico" }]],
-  locales: {
-    root: {
-      label: "中文",
-      lang: "zh",
-      title: "Zexin Yuan's blog",
-      description: "Technique && Life",
-      themeConfig: {
-        author: "Zexin Yuan",
+export default withMermaid(
+  defineConfigWithTheme<Config>({
+    head: [["link", { rel: "icon", href: "/logo.ico" }]],
+    markdown: {
+      math: true,
+    },
+    locales: {
+      root: {
+        label: "中文",
+        lang: "zh",
+        title: "Zexin Yuan's blog",
+        description: "Technique && Life",
+        themeConfig: {
+          author: "Zexin Yuan",
+        },
       },
     },
-  },
-})
+  })
+)
